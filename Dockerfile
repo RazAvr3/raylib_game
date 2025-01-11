538f0092d899c1ec641e52d963439ba845338034
RazAvr3 <109036359+RazAvr3@users.noreply.github.com>
2025-01-11 15:49:58 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Use an official Ubuntu as a base image
+FROM ubuntu:latest
+
+# Set non-interactive frontend for apt to avoid prompts
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Update package list and install required packages
+RUN apt-get update && apt-get install -y \
+    libasound2-dev \
+    libx11-dev \
+    libxrandr-dev \
+    libxi-dev \
+    libgl1-mesa-dev \
+    libglu1-mesa-dev \
+    libxcursor-dev \
+    libxinerama-dev \
+    libwayland-dev \
+    libxkbcommon-dev \
+    build-essential \
+    cmake \
+    clang \
+    clang-tidy \
+    clang-foramt
+
+COPY . /workspace
+
+WORKDIR /workspace
+
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "raylib.h"
+#include <vector>
+#include <ranges>
+#include <iostream>
+
+
+
+int main() {
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Raylib - Basic Window Example!!!!!!!!!!!!!!!!!!!!!!");
+
+    SetTargetFPS(60); // Set the game to run at 60 frames per second
+
+    // Main game loop
+    while (!WindowShouldClose()) { // Detect window close button or ESC key
+        // Start drawing
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE); // Clear the screen with a white background
+
+        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY); // Display text
+
+        EndDrawing();
+    }
+
+    // De-initialization
+    CloseWindow(); // Close window and unload resources
+
+    return 0;
+}
